7fae9dae1299e28866245e096a8c450aaa8348d4
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-07-21 05:40:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,9 +17,22 @@ const sock = io("http://www.windows93.net:8081", {
 })
  
 sock.on("connect", () => {
+socket.emit("user joined", "←", "red")
+socket.emit("messafe", "leavebot")
+socket.on('user left', function (data) {
+    //  if (data.toString().toLowerCase() == "?bot, hey") {
+      socket.emit("message",data.nick + "has left teh trollbox");)
+    //  }
+      }
 socket.on('message', function (data) {
-      if (data.toString().toLowerCase() == "?bot, hey") {
-      socket.emit("message","heyo"+Math.floor(Math.random() * 200);)
+     if (data.toString().toLowerCase() == "?&*(&secret") {
+      socket.emit("message","shit and piss basically. made by sah (speedyplane2247#6969 on diksord, homevar: YTJ)")
+     }
       }
+ socket.on('message', function (data) {
+     if (data.toString().toLowerCase() == "peace is dedicated") {
+      socket.emit("message","shoutouts to guys : darkok, nutshoe, domenic, alexapps, franx1024, 1024x2, speedyplane (me). also noxx is fucking cringe, can u 93fags fuck off, & dfu for ownership!")
+     }
       }
+
 })
